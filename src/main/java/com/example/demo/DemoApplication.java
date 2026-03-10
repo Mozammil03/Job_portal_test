2d7ccb5a76b7a1c1f2087b0578cdba3fb3826b56
Mozammil <alammozammil03@gmail.com>
2026-03-10 21:32:45 +0530

    update

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,7 +10,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		//test(ignore)1000
+		//test(ignore)100000
 	}
 
 }
